da6277a8f96164e2a5540ce4567a10a3b23fa5cb
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-02-05 19:24:54 -0200

    Aula 07. Apredendo sobre o Objeto global e Process

--- node-examples/primeiro-script.js
+++ node-examples/primeiro-script.js
@@ -1,9 +1,17 @@
 console.log('n-fatorial')
 
+/* console.log(`Executando o script a partir do diretório ${process.cwd()}`)
+
+process.on('exit',()=>{
+    console.log('Script está prestes a terminar');
+})
+ */
 const fatorial = (num) => {
     if(num === 0){
         return 1;
     }
     return num * fatorial (num -1 )
 }
-console.log(`O fatorial de 5 é igual a ${fatorial(5)}`);
+const num = parseInt(process.argv[2])
+
+console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`);
