19379932345537c9adcac1980af718cb335d3c2b
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-02-08 18:46:38 -0200

    Aula 19. Escrevendo o primeiro Recurso REST - (GET)

--- meat-api/server/server.ts
+++ meat-api/server/server.ts
@@ -1,9 +1,10 @@
 import * as restify from 'restify';
 import {environment} from '../common/environment'
+import {Router} from '../common/router'
 
 export class Server{
     application:restify.Server
-    initRoutes():Promise<any>{
+    initRoutes(routers:Router[]):Promise<any>{
         return new Promise((resolve,reject) =>{
             try{
                 this.application = restify.createServer({
@@ -11,32 +12,13 @@ export class Server{
                     version:'1.0.0'
                 });
                 
-                this.application.use(restify.plugins.queryParser())                
+                this.application.use(restify.plugins.queryParser());
 
-                this.application.get('/info',[
-                    (req, resp, next) =>{
-                        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
-                            //resp.status(400);
-                            //resp.json({message:'Please, update your browser'})
-                            let error:any = new Error()
-                            error.statusCode = 400;
-                            error.message = 'Please, update your browser'
-                            return next(error)
-                        }
-                       return next() 
-                    },
-                    (req, resp, next) =>{
-                    resp.json({
-                        browser:req.userAgent(),
-                        method:req.method,
-                        url:req.href(),
-                        path:req.path(),
-                        query:req.query,
-                    })
-                    return next()
-                }]);
+                for (const router of routers) {
+                    router.applyRoutes(this.application)                    
+                }
 
-                this.application.listen(environment.server.port, () => {
+                this.application.listen(environment.server.port , () => {
                     resolve(this.application)
                 })
                 
@@ -45,7 +27,7 @@ export class Server{
             }
         })
     }
-    bootstrap(): Promise<Server>{
-        return this.initRoutes().then(()=>this)
+    bootstrap(routers:Router[] = []): Promise<Server>{
+        return this.initRoutes(routers).then(()=>this)
     }
 }
