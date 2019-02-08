a1138bf4fb64253f02c057b6e5c2efcfe0f53019
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-02-08 11:05:34 -0200

    Aula 15. Apredendo sobre os Objetos Request, Response e Next

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -4,10 +4,30 @@ const server = restify.createServer({
     version:'1.0.0'
 });
 
-server.get('/hello',(req, resp, next) =>{
-    resp.json({message:'hello'})
+server.use(restify.plugins.queryParser())
+
+server.get('/info',[
+    (req, resp, next) =>{
+        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+            //resp.status(400);
+            //resp.json({message:'Please, update your browser'})
+            let error:any = new Error()
+            error.statusCode = 400;
+            error.message = 'Please, update your browser'
+            return next(error)
+        }
+       return next() 
+    },
+    (req, resp, next) =>{
+    resp.json({
+        browser:req.userAgent(),
+        method:req.method,
+        url:req.href(),
+        path:req.path(),
+        query:req.query,
+    })
     return next()
-});
+}]);
 
 server.listen(3000, () => {
     console.log('API is running on http://localhost:3000/hello')
