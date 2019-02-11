d91da15ae188ea0b3fe6ca85e3712b9c91f88d2b
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-02-10 23:13:54 -0200

    Aula 20. Obtendo um unico recurso (GET)

--- meat-api/users/users.model.ts
+++ meat-api/users/users.model.ts
@@ -1,10 +1,15 @@
 const users = [
-    {name:'Peter Parker',email:'peter@marvel.com'},
-    {name:'Bruce Wayne',email:'bruce@dc.com'}
+    {id:'1', name:'Peter Parker',email:'peter@marvel.com'},
+    {id:'2', name:'Bruce Wayne',email:'bruce@dc.com'}
 ]
 
 export class User {
     static findAll():Promise<any[]>{
         return Promise.resolve(users)
+    }
+    static findById(id):Promise<any>{
+        return new Promise(resolve =>{
+            resolve(users.find(user => user.id === id) || undefined)
+        })
     }
 }
