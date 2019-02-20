207f7ed3d2644fe19de613644b398da547b4c570
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-02-20 12:36:52 -0300

    Aula 31. Excluindo um Documento via DELETE

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -60,6 +60,17 @@ class UsersRouter extends Router{
                 return next()
             })
         })
+        application.del('/users/:id',(req, resp,next) =>{
+            User.remove({_id:req.params.id}).exec().then((cmdResult:any)=>{
+                if(cmdResult.result.n){
+                    resp.send(204)
+                    return next()
+                }else{
+                    resp.send(404)
+                }
+                return next()
+            })
+        })
     }
     
 } 
