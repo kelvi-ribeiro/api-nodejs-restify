73f1791b34c0a78484233bccf97d166f0b537fd9
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-02-25 15:27:09 -0300

    Aula 37. Validacoes e Middleware em Update (Mongoose)

--- meat-api/users/users.model.ts
+++ meat-api/users/users.model.ts
@@ -41,17 +41,36 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
-userSchema.pre('save',function(next){
-    const user:User = this
-    if(!user.isModified('password')){
+
+const hashPassword = (obj, next) =>{
+    bcrypt.hash(obj.password, environment.security.saltRounds)
+    .then(hash =>{
+        obj.password = hash
+        next()
+    }).catch(next)
+}
+
+const saveMiddleware = function(next){
+    userSchema.pre('save',function(next){
+        const user:User = this
+        if(!user.isModified('password')){
+            next()
+        }else{
+            hashPassword(user,next)
+        }
+    })
+}
+
+const updateMiddleware = function(next){
+    if(!this.getUpdate().password){
         next()
     }else{
-        bcrypt.hash(user.password, environment.security.saltRounds)
-        .then(hash =>{
-            user.password = hash
-            next()
-        }).catch(next)
+        hashPassword(this.getUpdate(),next)
     }
-})
+}
+
+userSchema.pre('save', saveMiddleware)
+userSchema.pre('findOneAndUpdate', updateMiddleware)
+userSchema.pre('update', updateMiddleware)
 
 export const User = mongoose.model<User>('User',userSchema)
