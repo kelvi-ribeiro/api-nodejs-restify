8ad28e74a8d861896c5e06cccbf32f4effabf259
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-03-09 04:33:03 -0300

    Aula 49. Testes Com POST e GET no Recurso de Users

--- meat-api/users/users.test.ts
+++ meat-api/users/users.test.ts
@@ -1,5 +1,49 @@
 import 'jest'
+import * as request from 'supertest'
+import {Server} from '../server/server'
+import {environment} from '../common/environment'
+import {usersRouter} from './users.router'
+import {User} from './users.model'
+let address:string
+let server:Server
+beforeAll(()=>{
+  environment.db.url = process.env.DB_URL || 'mongodb://localhost/meat-api-test-db'
+  environment.server.port = process.env.SERVER_PORT || 3001
+  address = `http://localhost:${environment.server.port}`
+  server = new Server()
+  return server.bootstrap([usersRouter])
+              .then(()=>User.remove({})
+              .exec())              
+              .catch(console.error)
+})
+test('get /users',()=>{
+    return request(address)
+    .get('/users')
+    .then(response =>{
+     expect(response.status).toBe(200)
+     expect(response.body.items).toBeInstanceOf(Array)
+    }).catch(fail)
+})
 
-test('primeiro teste',()=>{
-    
+test('post /users',()=>{
+    return request(address)
+    .post('/users')
+    .send({
+        name:'usuario1',
+        email:'usuario1@email.com',
+        password:'123456',
+        cpf:'951.591.910-03'
+    })
+    .then(response =>{
+     expect(response.status).toBe(200)
+     expect(response.body._id).toBeDefined()
+     expect(response.body.name).toBe('usuario1')
+     expect(response.body.email).toBe('usuario1@email.com')
+     expect(response.body.cpf).toBe('951.591.910-03')
+     expect(response.body.password).toBeUndefined()
+    }).catch(fail)
+})
+
+afterAll(()=>{
+    return server.shutdown()
 })
