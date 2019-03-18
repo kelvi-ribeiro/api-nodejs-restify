9e2c2e20a64f758b70e2fab248ac0b5a12d24a0e
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-03-17 22:51:58 -0300

    Aula 62. Logging com Restify e PM2

--- meat-api/server/server.ts
+++ meat-api/server/server.ts
@@ -1,61 +1,86 @@
-import * as restify from 'restify'
 import * as fs from 'fs'
+
+import * as restify from 'restify'
+import * as mongoose from 'mongoose'
+
 import {environment} from '../common/environment'
 import {logger} from '../common/logger'
 import {Router} from '../common/router'
-import * as mongoose from 'mongoose'
 import {mergePatchBodyParser} from './merge-patch.parser'
-import {handleError} from './error-handler'
-import { tokenParser } from '../security/token.parser'
+import {handleError} from './error.handler'
 
-export class Server{
-    application:restify.Server
+import {tokenParser} from '../security/token.parser'
 
-    initializeDb():mongoose.MongooseThenable{
-        (<any>mongoose).Promise = global.Promise
-        return mongoose.connect(environment.db.url,{
-            useMongoClient:true
+export class Server {
+
+  application: restify.Server
+
+  initializeDb(): mongoose.MongooseThenable {
+    (<any>mongoose).Promise = global.Promise
+    return mongoose.connect(environment.db.url, {
+      useMongoClient: true
+    })
+  }
+
+  initRoutes(routers: Router[]): Promise<any>{
+    return new Promise((resolve, reject)=>{
+      try{
+
+        const options: restify.ServerOptions = {
+          name: 'meat-api',
+          version: '1.0.0',
+          log: logger
+        }
+        if(environment.security.enableHTTPS){
+          options.certificate = fs.readFileSync(environment.security.certificate),
+          options.key = fs.readFileSync(environment.security.key)
+        }
+
+        this.application = restify.createServer(options)
+
+        this.application.pre(restify.plugins.requestLogger({
+          log: logger
+        }))
+
+        this.application.use(restify.plugins.queryParser())
+        this.application.use(restify.plugins.bodyParser())
+        this.application.use(mergePatchBodyParser)
+        this.application.use(tokenParser)
+
+        //routes
+        for (let router of routers) {
+          router.applyRoutes(this.application)
+        }
+
+        this.application.listen(environment.server.port, ()=>{
+           resolve(this.application)
         })
-    }
-    initRoutes(routers:Router[]):Promise<any>{
-        return new Promise((resolve,reject) =>{
-            try{
-                const options:restify.ServerOptions = {
-                name:'meat-api',
-                version:'1.0.0',    
-                }
-                if(environment.security.enableHTTPS){
-                    options.certificate = fs.readFileSync(environment.security.certificate),
-                    options.key = fs.readFileSync(environment.security.key)
-                }
-                this.application = restify.createServer(options)
-                
-                this.application.use(restify.plugins.queryParser())
-                this.application.use(restify.plugins.bodyParser())
-                this.application.use(mergePatchBodyParser)
-                this.application.use(tokenParser)
 
-                for (const router of routers) {
-                    router.applyRoutes(this.application)                    
-                }
+        this.application.on('restifyError', handleError)
+        //(req, resp, route, error)
+        /*this.application.on('after', restify.plugins.auditLogger({
+          log: logger,
+          event: 'after',
+          server: this.application
+        }))
 
-                this.application.listen(environment.server.port , () => {
-                    resolve(this.application)
-                })
+        this.application.on('audit', data=>{
 
-                this.application.on('restifyError',handleError)
-                
-            }catch(error){
-            reject(error)
-            }
-        })
-    }
-    bootstrap(routers:Router[] = []): Promise<Server>{
-        return this.initializeDb().then(() => 
-            this.initRoutes(routers).then(()=>this)
-        )
-    }
-    shutdown(){
-        return mongoose.disconnect().then(()=>this.application.close())
-    }
+        })*/
+
+      }catch(error){
+        reject(error)
+      }
+    })
+  }
+
+  bootstrap(routers: Router[] = []): Promise<Server>{
+      return this.initializeDb().then(()=>
+             this.initRoutes(routers).then(()=> this))
+  }
+
+  shutdown(){
+    return mongoose.disconnect().then(()=>this.application.close())
+  }
+
 }
